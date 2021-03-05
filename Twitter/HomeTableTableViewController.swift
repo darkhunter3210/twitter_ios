442c73c1bf5fa1905a966d7742b0ddd909dd57cb
darkhunter3210 <matthewcheung3003@gmail.com>
2021-03-04 23:08:05 -0500

    Finish Twitter Part 1

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Matthew Cheung on 3/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberofTweet: Int!
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+       
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+            cell.profileimageView.image = UIImage(data:imageData)
+        }
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweet()
+    }
+    
+    @IBAction func onLongout(_ sender: Any) {
+        
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    func loadTweet()
+    {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { ( tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets
+            {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+        }, failure: { (Error) in
+            print("Could not retreive tweets! oh no!!")
+        })
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweetArray.count
+    }
+
+  
+
+}
---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Matthew Cheung on 3/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileimageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
